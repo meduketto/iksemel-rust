4d7555be852b1207d12df3c66e22b07c96a89629
Gurer Ozen <meduketto@gmail.com>
2025-07-19 14:56:31 -0400

    more sax code

--- src/parser.rs
+++ src/parser.rs
@@ -17,26 +17,31 @@ enum State {
     Whitespace,
 }
 
-pub struct Parser {
-    state: State,
-    old_state: State,
-    nr_bytes: usize,
-    nr_lines: usize,
-}
-
 pub enum ParserError {
     NoMemory,
     BadXml,
+    HandlerError,
 }
 
-pub enum Element {
-    NotYet,
-    StartTag,
-    Attribute,
-    EndTag,
-    CData,
-    Comment,
-    PI,
+pub enum SaxElement<'a> {
+    StartTag(&'a str),
+    Attribute(&'a str, &'a str),
+    EmptyElementTag,
+    EndTag(&'a str),
+    CData(&'a str),
+    Comment(&'a str),
+    PI(&'a str),
+}
+
+pub trait SaxHandler {
+    fn handle_element(&mut self, element: SaxElement) -> Result<(), ParserError> { Ok(()) }
+}
+
+pub struct Parser {
+    state: State,
+    nr_bytes: usize,
+    nr_lines: usize,
+    buffer: Vec<u8>,
 }
 
 macro_rules! whitespace {
@@ -45,17 +50,19 @@ macro_rules! whitespace {
     };
 }
 
+const INITIAL_BUFFER_CAPACITY: usize = 128;
+
 impl Parser {
     pub fn new() -> Parser {
         Parser {
             state: State::Prolog,
-            old_state: State::Prolog,
             nr_bytes: 0,
             nr_lines: 0,
+            buffer: Vec::<u8>::with_capacity(INITIAL_BUFFER_CAPACITY),
         }
     }
 
-    pub fn parse_bytes(&mut self, bytes: &[u8]) -> Result<Element, ParserError> {
+    pub fn parse_bytes(&mut self, handler: &mut impl SaxHandler, bytes: &[u8]) -> Result<(), ParserError> {
         let mut pos: usize = 0;
         let mut back: usize = 0;
 
@@ -73,13 +80,20 @@ impl Parser {
                 State::PrologTag => match c {
                     b'!' => (),
                     b'?' => self.state = State::PI,
-                    _ => self.state = State::TagName,
+                    _ => {
+                        back = pos;
+                        self.state = State::TagName;
+                    },
                 },
 
                 State::TagName => match c {
-                    b'/' => (),
-                    b'>' => (),
-                    whitespace!() => (),
+                    b'/' | b'>' | whitespace!() => {
+                        if back < pos {
+                            self.buffer.extend_from_slice(&bytes[back..pos]);
+                        }
+                        handler.handle_element(SaxElement::StartTag("lala"))?;
+                        self.buffer.clear();
+                    },
                     _ => (),
                 },
 
@@ -95,7 +109,14 @@ impl Parser {
             }
         }
 
-        Ok(Element::NotYet)
+        if back < pos  {
+            match self.state {
+                State::TagName => self.buffer.extend_from_slice(&bytes[back..pos]),
+                _ => (),
+            }
+        }
+
+        Ok(())
     }
 
     pub fn nr_bytes(&self) -> usize {
@@ -111,11 +132,49 @@ impl Parser {
 mod tests {
     use super::*;
 
+    struct TestHandler<'a> {
+        expected: &'a [SaxElement<'a>],
+        current: usize,
+    }
+
+    impl<'a> TestHandler<'a> {
+        fn new(expected: &'a [SaxElement]) -> TestHandler<'a> {
+            TestHandler {
+                expected,
+                current: 0,
+            }
+        }
+
+        fn check(&mut self, s: &str) {
+            let mut parser = Parser::new();
+            assert!(parser.parse_bytes(self, &s.as_bytes()).is_ok());
+            assert_eq!(self.current, self.expected.len());
+        }
+    }
+
+    impl<'a> SaxHandler for TestHandler<'a> {
+        fn handle_element(&mut self, element: SaxElement) -> Result<(), ParserError> {
+            if self.current >= self.expected.len() {
+                return Err(ParserError::HandlerError);
+            }
+            match self.expected[self.current] {
+                SaxElement::StartTag(s) => (), // Check tag name
+                _ => return Err(ParserError::HandlerError),
+            }
+            self.current += 1;
+            Ok(())
+        }
+    }
+
     #[test]
     fn it_works() {
-        let mut parser = Parser();
+        TestHandler::new(&[SaxElement::StartTag("a")]).check("<a>lala</a>");
     }
 }
+
+// FIXME: finish testing util
+
+// FIXME: nr_column
 
 // FIXME: intake type for parse() str? io?
 // FIXME: return value for parse()
