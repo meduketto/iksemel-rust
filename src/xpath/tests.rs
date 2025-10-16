29575495de31d882467add6ad0daf5a2bba11920
Gurer Ozen <meduketto@users.noreply.github.com>
2025-10-16 19:47:34 -0400

    xpath copyrights do not extend to 2000s (#41)

--- src/xpath/tests.rs
+++ src/xpath/tests.rs
@@ -1,6 +1,6 @@
 /*
 ** This file is a part of Iksemel (XML parser for Jabber/XMPP)
-** Copyright (C) 2000-2025 Gurer Ozen
+** Copyright (C) 2025 Gurer Ozen
 **
 ** Iksemel is free software: you can redistribute it and/or modify it
 ** under the terms of the GNU Lesser General Public License as
